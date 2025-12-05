6a6416a81568a2e4151b1276e61c0cf644101c95
songjj <shongjj@meleapcn.com>
2025-12-05 17:08:32 +0800

    Add camera management and QR code detection functionality with UI components

---
+++ LearnSwift/UIKits/VisonCamPre.swift
@@ -0,0 +1,30 @@
+//
+//  VisonCamPre.swift
+//  LearnSwift
+//
+//  Created by song on 2025/12/5.
+//
+
+import AVFoundation
+import SwiftUI
+import UIKit
+
+struct VisonCamPre: UIViewRepresentable {
+    
+    
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        view.backgroundColor = .black
+
+        let previewLayer = AVCaptureVideoPreviewLayer()
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = view.bounds
+        view.layer.addSublayer(previewLayer)
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        // 更新视图的逻辑
+    }
+}
